688861724f086aa29702e6a45f5b0ac69e992e82
YuvrajBisht17 <yuvisingh1710@gmail.com>
2026-02-12 06:00:36 +0000

    new file

---
+++ array.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void main()
+{
+int a[10],i;
+printf("enter 10 elements \n");
+for (i = 0;i <= 9;i++)
+{
+    scanf("%d",&a[i]);
+}
+for (int i = 0; i <= 9; i++) {
+     printf("%d ", a[i]);
+}
+}
